68c223ac2a779a9f796dace06482713ff3ca4c32
Alexandertsvet <tsvetkov1986@mail.ru>
2024-12-12 15:53:56 +0300

    spark_3.ipynb

---
+++ ETL/init_spark_env.py
@@ -0,0 +1,14 @@
+import os, sys
+os.environ['SPARK_HOME'] = '/home/alexandr/stydy/eda/venv/lib/python3.10/site-packages/pyspark'
+sys.path.append('/home/alexandr/stydy/eda/venv/lib/python3.10/site-packages/pyspark/python/lib/py4j-0.10.9.7-src.zip')
+sys.path.append('/home/alexandr/stydy/eda/venv/lib/python3.10/site-packages/pyspark/python')
+
+['',
+ '/tmp/spark-6be4ded7-5301-4022-b1d9-1f05604a4cd8/userFiles-385f6327-5ab3-4f28-a306-0e1032bde3ea',
+ '/home/alexandr/stydy/eda/venv/lib/python3.10/site-packages/pyspark/python/lib/py4j-0.10.9.7-src.zip',
+ '/home/alexandr/stydy/eda/venv/lib/python3.10/site-packages/pyspark/python',
+ '/home/alexandr/stydy/eda',
+ '/usr/lib/python310.zip',
+ '/usr/lib/python3.10',
+ '/usr/lib/python3.10/lib-dynload',
+ '/home/alexandr/stydy/eda/venv/lib/python3.10/site-packages']
